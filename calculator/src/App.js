8cae9161ba3304f5f7e19055b01a7289ec8bfade
Josmar Diaz <jdm.begreat28@gmail.com>
2023-07-27 19:18:12 -0400

    Created Function to Create Digits 1-9 on Button

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,4 +1,16 @@
 function App() {
+
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button key={i}>{i}</button>
+      )
+    }
+
+    return digits;
+  }
   return (
     <div className="App">
       <div className="calculator">
@@ -16,6 +28,7 @@ function App() {
         </div>
 
         <div className="digits">
+          {createDigits()}
           <button>0</button>
           <button>.</button>
           <button>=</button>
